c1a49c0d085ab313f566588fe59f57ca88882037
Costindumitru87 <47321123+Costindumitru87@users.noreply.github.com>
2021-06-07 13:45:36 +0300

    Create CASE_WHEN_statement.sql

---
+++ slqResources/CASE_WHEN_statement.sql
@@ -0,0 +1,35 @@
+### Basic CASE statement example.
+-- Identify the home team as Bayern Munich, Schalke 04, or neither
+SELECT 
+	CASE WHEN hometeam_id = 10189 THEN 'FC Schalke 04'
+        WHEN hometeam_id = 9823 THEN 'FC Bayern Munich'
+         ELSE 'Other' END AS home_team,
+	COUNT(id) AS total_matches
+FROM matches_germany
+-- Group by the CASE statement alias
+GROUP BY home_team;
+
+
+### Filtering a CASE statement example
+SELECT *
+FROM table
+WHERE 
+    CASE WHEN a > 5 THEN 'Keep'
+         WHEN a <= 5 THEN 'Exclude' 
+         END = 'Keep';
+         
+         
+### Calculating percent with CASE and AVG example
+SELECT 
+	c.name AS country,
+    -- Round the percentage of tied games to 2 decimal points
+	ROUND(AVG(CASE WHEN m.season='2013/2014' AND m.home_goal = m.away_goal THEN 1
+			 WHEN m.season='2013/2014' AND m.home_goal != m.away_goal THEN 0
+			 END),2) AS pct_ties_2013_2014,
+	ROUND(AVG(CASE WHEN m.season='2014/2015' AND m.home_goal = m.away_goal THEN 1
+			 WHEN m.season='2014/2015' AND m.home_goal != m.away_goal THEN 0
+			 END),2) AS pct_ties_2014_2015
+FROM country AS c
+LEFT JOIN matches AS m
+ON c.id = m.country_id
+GROUP BY country;
