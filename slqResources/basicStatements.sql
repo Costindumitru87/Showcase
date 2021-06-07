ba24a7e17f0a3273e6c4590c18e486f275f0fba6
Costindumitru87 <47321123+Costindumitru87@users.noreply.github.com>
2021-06-07 14:27:50 +0300

    Update basicStatements.sql

--- slqResources/basicStatements.sql
+++ slqResources/basicStatements.sql
@@ -1,39 +1,46 @@
 ### Basic select
 ```SELECT * 
-FROM  mytable;```
+FROM  mytable;
+```
 
 
 ### Select columns
 ```SELECT col1, col2
-FROM mytable;``` 
+FROM mytable;
+``` 
 
 
 ### Query data and filter rows with a condition
 ```SELECT c1, c2 
 FROM mytable
-WHERE condition;```
+WHERE condition;
+```
 
 
 ### Query distinct rows from a table
 ```SELECT DISTINCT c1
 FROM mytable 
-WHERE condition;```
+WHERE condition;
+```
 
 
 ### Sort the result set in ascending or descending order
 ```SELECT c1, c2 
 FROM mytable
-ORDER BY c1 ASC [DESC];```
+ORDER BY c1 ASC [DESC];
+```
 
 
 ### Group rows using an aggregate function (AVG, COUNT, SUM, MAX, MIN)
 ```SELECT c1, SUM(c2)
 FROM mytable
-GROUP BY c1;```
+GROUP BY c1;
+```
 
 
 ### Filter groups using HAVING clause
 ```SELECT c1, SUM(c2)
 FROM mytable
 GROUP BY c1
-HAVING condition;```
+HAVING condition;
+```
