b1dd4b95dd54d08e688f9ace9654a2f980736432
Costindumitru87 <47321123+Costindumitru87@users.noreply.github.com>
2021-06-08 11:09:43 +0300

    Update UDFs.sql

--- slqResources/UDFs.sql
+++ slqResources/UDFs.sql
@@ -1,24 +1,24 @@
 ### CREATE/REPLACE/ALTER/DROP User-Defined Function
-```CREATE FUNCTION [database_name.]function_name (parameters)
+CREATE FUNCTION [database_name]function_name (parameters)
 RETURNS data_type AS
 BEGIN
     SQL statements
     RETURN value
 END;
-```
-```ALTER FUNCTION [database_name.]function_name (parameters)
+
+ALTER FUNCTION [database_name]function_name (parameters)
 RETURNS data_type AS
 BEGIN
     SQL statements
     RETURN value
 END;
-```    
-```DROP FUNCTION [database_name.]function_name;
-```
+    
+DROP FUNCTION [database_name.]function_name;
+
 
 
 ### Transformation function example in BQ
-```create or replace function `test.NBA_transform.fn_teams`(data string)
+create or replace function `test.NBA_transform.fn_teams`(data string)
 returns struct <      
         data array<struct<
             city string,
@@ -40,12 +40,11 @@ returns struct <
 >  
 
 language js as """ return JSON.parse(data);""";
-```
 
-# Create view
-```sql
+
+### Create view
+
 create or replace view `test.NBA_transform.teams_raw` as
 select `test.NBA_transform.fn_teams`(concat('''{"data":''',data,'''}''')).*  
 from `test.NBA_import.teams_import`;
-```
 
