c803f7a1e62be85105e52efe1eaed679b4efff76
Costindumitru87 <47321123+Costindumitru87@users.noreply.github.com>
2021-06-10 11:36:27 +0300

    Update Loops.py

--- pythonResources/Loops.py
+++ pythonResources/Loops.py
@@ -27,7 +27,7 @@ while current_number <= 10:
 pets = ['dog', 'cat', 'dog', 'fish', 'cat', 'rabbit', 'cat']
 print(pets)
 while 'cat' in pets:
-   pets.remove('cat')
+  pets.remove('cat')
 print(pets)
 
   
